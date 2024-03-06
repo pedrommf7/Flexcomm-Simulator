f6a85cf675b800f10b0a57a6a985c885658a180a
pedrommf7 <pedro2mferreira@gmail.com>
2024-03-06 12:10:31 +0000

    prevent negative free values

--- ns-3.35/src/link-stats/model/link-stats.cc
+++ ns-3.35/src/link-stats/model/link-stats.cc
@@ -72,9 +72,10 @@ LinkStats::LogStatsInternal (Ptr<OutputStreamWrapper> streamWrapper)
 
 
   std::ostream *stream = streamWrapper->GetStream ();
+  double usage = 1-m_channel->GetChannelUsage ();
   *stream << std::fixed << Simulator::Now ().GetSeconds () << ";" << linkName << ";"
           << m_channel->GetChannelUsage ()
-          << ";" << m_channel->GetDataRate().GetBitRate() * (1-m_channel->GetChannelUsage ())
+          << ";" << m_channel->GetDataRate().GetBitRate() * (usage>=1?1:1-usage)
           << ";" << Names::FindName (src) << ";" << Names::FindName (dst)
           << "\n";
 }
