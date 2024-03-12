b831bc6d27bc1998050ec4bcb7fc006bfe343618
pedrommf7 <pedro2mferreira@gmail.com>
2024-03-12 10:58:40 +0000

    enable flow monitor

--- many_tests.sh
+++ many_tests.sh
@@ -8,10 +8,10 @@
 # done
 
 
-#link_capacitys=("100Mb" "240Mb" "1000Mb" "1Gb" "2,4Gb" "10Gb")
+link_capacitys=("100Mb" "240Mb" "1000Mb" "1Gb" "2,4Gb" "10Gb")
 #link_capacitys=("100Mb" "240Mb" "1000Mb")
-# link_capacitys=("1Gb" "2,4Gb" "10Gb")
-link_capacitys=("11Gb" "15Gb" "20Gb")
+#link_capacitys=("1Gb" "2,4Gb" "10Gb")
+#link_capacitys=("11Gb" "15Gb" "20Gb")
 
 for link_capacity in "${link_capacitys[@]}"
 do
